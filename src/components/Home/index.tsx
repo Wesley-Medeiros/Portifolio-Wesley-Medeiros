123b55d6917e3d881e3c70adefd4151ceea3264c
Wesley Medeiros Ramalho <wesley.medeiros6938@gmail.com>
2023-04-18 20:43:50 -0300

    Estruturação da área About

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,31 +1,69 @@
-import { Button, GithubButton, HomeContainer, HomeLeft, LikedinButton, SpanCursor, SpanSentence } from "./styles"
+import { MainButton, HomeContainer, HomeLeft, SpanCursor, SpanSentence, HomeRight, AboutContainer, Svg, DescriptionContainer, AboutButtons } from "./styles"
 import  linkedin  from "../../assets/linkedin.png"
 import  github  from "../../assets/github.png"
+import DevCoding from "../../assets/illustration-coding.svg"
+import Profile from "../../assets/illustration-profile.svg"
+import curriculum from "../../assets/Currículo Wesley Medeiros.pdf"
+import cloud from "../../assets/download-da-nuvem.png"
+import email from "../../assets/email.png"
 
 function Home() {
   return(
+  <>
     <HomeContainer>
        <HomeLeft >
           <h1>Olá, eu sou</h1>
             <SpanSentence></SpanSentence>
             <SpanCursor></SpanCursor>
-          <h1>Desenvolvedora Front-End</h1>
-          <Button>
+          <h1>Desenvolvedor Front-End</h1>
+          <MainButton>
             <a href="https://www.linkedin.com/in/wesley-medeiros-694809196/" target="_blank">
-              <LikedinButton>
                 <img src={linkedin} alt="" />
                 <p>LinkedIn</p>
-              </LikedinButton>
             </a>
             <a href="https://github.com/Wesley-Medeiros" target="_blank">
-              <GithubButton>
                 <img src={github} alt="" />
                 <p>Github</p>
-              </GithubButton>
             </a>
-          </Button>
+          </MainButton>
         </HomeLeft>
+        <HomeRight>
+          <img src={DevCoding} alt="Ilustração representando uma desenvolvedor sentada na cadeira mexendo no notebook com ambiente em volta." data-tilt/>
+        </HomeRight> 
     </HomeContainer>
+    <AboutContainer>
+    <Svg>
+      <img src={Profile} alt="Uma foto de Wesley Medeiros" />
+    </Svg>
+    <DescriptionContainer>
+      <h1>Sobre mim</h1>
+      <h2>Recife, Pernambuco</h2>
+      <p>
+        Olá! Eu sou um estudante de ADS em busca de aprendizado e crescimento que está sempre disposto
+        a fazer o possível para garantir sucesso em seus objetivos. Atualmente com foco na
+        expansão de conhecimentos em atuações de projetos ligados a desenvolvimento Front End.
+      </p>
+      <AboutButtons>
+        <a href={curriculum} download="">
+          <div >
+            <img src={cloud} alt="" />
+            <p>Currículo</p>
+          </div>
+        </a>
+        <a
+          href="mailto: wmedeiros.dev@gmail.com?subject=Wesley, vim pelo seu portfólio! :)"
+          target="_blank"
+        >
+          <div>
+            <img src={email} alt="" />
+            <p>E-mail</p>
+          </div>
+        </a>
+      </AboutButtons>
+      
+    </DescriptionContainer>
+    </AboutContainer>
+    </>
   )
 }
 
--- src/components/Home/styles.ts
+++ src/components/Home/styles.ts
@@ -6,6 +6,7 @@ export const HomeContainer = styled.main`
   align-items: center;
   position: relative;
   min-height: 60vh;
+  padding: 6.5%;
 `
 
 export const HomeLeft = styled.div`
@@ -27,7 +28,10 @@ span {
 }
 `
 
-export const HomeRight = styled.div``
+export const HomeRight = styled.div`
+  width: 40%;
+  margin: 2rem 0;
+`
 
 export const SpanSentence = styled.span`
   width: 3px;
@@ -38,19 +42,109 @@ export const SpanSentence = styled.span`
   top: 3px;
   animation: blink 0.6s infinite alternate;
 `
-
 export const SpanCursor = styled.span``
 
-export const Button = styled.div`
+export const MainButton = styled.div`
   display: flex;
   width: 350px;
   justify-content: space-between;
+
+  img {
+    width: 22px;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 3px 5px 5px;
+    justify-content: space-evenly;
+    width: 140px;
+    border: 2px solid #00d2df;
+    border-radius: 8px;
+    position: relative;
+    background-color: #121212;
+    margin-top: 1rem;
+    transition: 0.3s;
+    font-weight: 600;
+    font-size: 0.95rem;
+    letter-spacing: 1.5px;
+    height: 40px; 
+  }
+
+  a:hover {
+    background-color: #272727;
+    text-shadow: 1px 1px 3px #00bbc5;
+  }
+`
+export const AboutContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  position: relative;
+  min-height: 60vh;
+  padding: 6.5%;
+  background-color: #151515;
+
+
+  h1 {
+    margin-bottom: 2rem;
+  }
+
+  h2 {
+    margin-bottom: 1rem;
+  }
+
+  p {
+    display: block;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+  }
 `
 
-export const LikedinButton = styled.div`
+export const Svg = styled.div`
+  min-width: 300px;
+  
+  img {
+    border-left: 5px solid #00d2df;
+    transition: 0.4s;   
+}
+`
+
+export const DescriptionContainer =styled.div`
+  line-height: 1.5rem;
+  width: 70%;
+  margin-top: 2rem;
   
 `
 
-export const GithubButton = styled.div`
-  
+export const AboutButtons =styled.div` 
+  display: flex;
+  width: 400px;
+  justify-content: space-between;
+  margin-top: 2rem;
+
+  img {
+    display: flex;
+    width: 22px;
+  }
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 3px 5px 5px;
+    justify-content: space-evenly;
+    width: 140px;
+    border: 2px solid #00d2df;
+    border-radius: 8px;
+    position: relative;
+    background-color: #121212;
+    margin-top: 1rem;
+    transition: 0.3s;
+    font-weight: 600;
+    font-size: 0.95rem;
+    letter-spacing: 1.5px;
+    height: 40px; 
+  }
 `
